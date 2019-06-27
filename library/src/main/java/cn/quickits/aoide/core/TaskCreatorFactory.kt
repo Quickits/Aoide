d4507219aaf732eba289e901fe37030d2da00016
taicheng <iamdennisme@gmail.com>
2019-06-27 12:54:29 +0800

    feat: support mp3 custom parameter

--- library/src/main/java/cn/quickits/aoide/core/TaskCreatorFactory.kt
+++ library/src/main/java/cn/quickits/aoide/core/TaskCreatorFactory.kt
@@ -5,12 +5,14 @@ import cn.quickits.aoide.converter.aac.AACFormatConverter
 import cn.quickits.aoide.converter.mp3.Mp3FormatConverter
 import cn.quickits.aoide.converter.pcm.PCMFormatConverter
 import cn.quickits.aoide.converter.wav.WAVFormatConverter
+import cn.quickits.aoide.recorder.AudioRecorder
 
 class TaskCreatorFactory(internal val context: Context) {
 
     fun aac() = TaskCreator(context, AACFormatConverter.create())
 
-    fun mp3() = TaskCreator(context, Mp3FormatConverter.create())
+    fun mp3(sampleRateInHz: Int = AudioRecorder.DEFAULT_SAMPLE_RATE, channels: Int = 1, bitsPerSample: Int = 16) =
+        TaskCreator(context, Mp3FormatConverter.create())
 
     fun wav() = TaskCreator(context, WAVFormatConverter.create())
 
